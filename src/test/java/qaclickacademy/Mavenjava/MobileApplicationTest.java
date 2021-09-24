998a33a530629198c0ff1e386276fcc864e7f420
jasmine <jasminedosani119@gmail.com>
2021-09-24 16:19:06 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/MobileApplicationTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class MobileApplicationTest {
+
+	
+	@Test
+	public void Mobiletestcase1()
+	{
+		System.out.println("Test case1 for Mobile application");
+	}
+	
+	@Test
+	public void Mobiletestcase2()
+	{
+		System.out.println("Test case2 for Mobile application");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/WebApplicationTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class WebApplicationTest {
+
+	
+	@Test
+	public void Webtestcase1()
+	{
+	System.out.println("Test case1 for Web application");	
+	}
+	
+	@Test
+	public void Webtestcase2()
+	{
+		System.out.println("Test case2 for Web application");		
+	}
+	
+}
